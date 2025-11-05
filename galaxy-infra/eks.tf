df08d7faaee97883eda14e586d0c5b259cf6f8ba
younis606 <younisosama460@gmail.com>
2025-11-05 12:13:35 +0200

    Create eks.tf

---
+++ galaxy-infra/eks.tf
@@ -0,0 +1,54 @@
+# EKS Cluster Configuration
+
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "${var.project_name}-eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+
+
+resource "aws_eks_cluster" "main" {
+  name     = "${var.project_name}-eks"
+  role_arn = aws_iam_role.eks_cluster_role.arn
+  version  = "1.30"
+
+  vpc_config {
+    subnet_ids = concat(
+      aws_subnet.public_subnets[*].id,
+      aws_subnet.private_subnets[*].id
+    )
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_policy
+  ]
+}
+
+
+
+
+data "aws_eks_cluster" "eks" {
+  name = aws_eks_cluster.main.name
+}
+
+data "aws_eks_cluster_auth" "eks" {
+  name = aws_eks_cluster.main.name
+}
